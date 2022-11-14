f3ee44f5efe89eb945d9e98247a9e139a1af2044
André Ferreira <andreferreira2302145@gmail.com>
2022-11-14 21:35:36 +0000

    Done_1_7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -32,6 +32,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {bad + good + neutral}</p>
+      <p>Average: {(good - bad) / (bad + good + neutral)}</p>
+      <p>Positive: {(good / (bad + good + neutral)) * 100} %</p>
     </div>
   );
 };
