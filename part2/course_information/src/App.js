f0df859e8e9c3b8f381186bf27beeaac3a28bd9a
André Ferreira <andreferreira2302145@gmail.com>
2022-11-18 15:13:17 +0000

    step6

--- part2/course_information/src/App.js
+++ part2/course_information/src/App.js
@@ -1,5 +1,42 @@
-function App() {
-  return <div className="App"></div>;
-}
+const Course = ({ course }) => {
+  return (
+    <div>
+      <h1 key={course.id}>{course.name}</h1>
+      {course.parts.map((item) => {
+        return (
+          <p key={item.id}>
+            {item.name} {item.exercises}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
 
 export default App;
