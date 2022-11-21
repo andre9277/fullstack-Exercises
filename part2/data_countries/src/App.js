4684c63cb476e50e52336971615423511ef9dafb
André Ferreira <andreferreira2302145@gmail.com>
2022-11-21 21:52:19 +0000

    Done 2.12

--- part2/data_countries/src/App.js
+++ part2/data_countries/src/App.js
@@ -1,5 +1,56 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleSearchCountry = (event) => {
+    setSearchCountry(event.target.value);
+  };
+
+  let countryfilt = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchCountry.toLowerCase())
+  );
+  return (
+    <div>
+      Find countries:{" "}
+      <input value={searchCountry} onChange={handleSearchCountry} />
+      <div>
+        {countryfilt.length > 10 ? (
+          <div> Too many matches, specify another filter!!</div>
+        ) : (
+          countryfilt.map((ct) => (
+            <div key={ct.name.common}>
+              {countryfilt.length === 1 ? (
+                <div>
+                  <h1>{ct.name.common}</h1>
+                  <div>Capital: {ct.capital}</div>
+                  <div>Area: {ct.area} </div>
+                  <div>
+                    Languages:
+                    <ul>
+                      {Object.values(ct.languages).map((item) => (
+                        <li key={item.length}>{item}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <img src={ct.flags.png} alt="not found" />
+                </div>
+              ) : (
+                <li key={ct.name.common}>{ct.name.common}</li>
+              )}
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 export default App;
